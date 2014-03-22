c18c83c43c91f8fd1bac57dfd9f1d7ba60693688
Henrik Karlsson <sven.henrik.karlsson@gmail.com>
2014-03-22 19:46:03 +0100

    Revert c2ace4c..371ca2f
    This rolls back to commit c2ace4cb92b07fb8678b81f19c67d06d556628e7.

--- Resources/app.js
+++ Resources/app.js
@@ -1,65 +1,196 @@
-/*
- * A master detail view, utilizing a native table view component and platform-specific UI and navigation.
- * A starting point for a navigation-based application with hierarchical data, or a stack of windows.
- * Requires Titanium Mobile SDK 1.8.0+.
- *
- * In app.js, we generally take care of a few things:
- * - Bootstrap the application with any data we need
- * - Check for dependencies like device type, platform version or network connection
- * - Require and open our top-level UI component
- *
- */
 var defaultFontSize = 14;
-//bootstrap and check dependencies
-if (Ti.version < 1.8) {
-  alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
+
+console.log("Erik, din kod laddas och funkar");
+ 
+// Set the background color to non-black to see the status bar
+// Or set the Window statusBarStyle property to a non-default value
+
+Ti.UI.setBackgroundColor('#232a35');
+ 
+function StyledWindow(title) {
+        var win = Ti.UI.createWindow({
+                // Remove the status bar
+                // fullscreen: true,
+                // Moves the Window below the status bar
+                statusBarStyle: Titanium.UI.iPhone.StatusBar.LIGHT_CONTENT,
+                top: 0
+        });
+       
+        return win;
+};
+ 
+var artWindow = Ti.UI.createView({
+        backgroundColor: '#232a35',
+        height: 59,
+        top: 0
+});
+ 
+var profileIcon = Ti.UI.createImageView({
+        image: 'img/profile@2x.png',
+        left:15, bottom: 14,
+        width:12, height:14
+});
+artWindow.add(profileIcon);
+ 
+var flowLabel = Ti.UI.createLabel({
+        color:'white',
+        font:{fontFamily:'Lato-Bold', fontSize:defaultFontSize+2},
+        text:'Flöde',
+        textAlign: 'center',
+        bottom: 14
+});
+artWindow.add(flowLabel);
+ 
+var win = new StyledWindow('Articles');
+ 
+win.add(artWindow);
+ 
+var searchArea = Ti.UI.createView({
+        backgroundColor: '#fff',
+        font:{fontFamily:'Lato-Regular', fontSize:defaultFontSize+4},
+        height: 161,
+        top: 59
+});
+ 
+var searchBar = Ti.UI.createSearchBar({
+        barColor:'#fff',
+        font:{fontFamily:'Lato-Regular', fontSize:defaultFontSize+4},
+        color: '#858688',
+        showCancel:true,
+        top: 0,
+        height:48,
+        value: 'Sök dryck…'
+});
+searchArea.add(searchBar);
+ 
+var searchCategories = Ti.UI.createLabel({
+        color:'#858688',
+        font:{fontFamily:'Lato-Regular', fontSize:defaultFontSize+3},
+        text:'Kategorier',
+        top: 49, left: 10,
+        height: 48,
+        textAlign: 'left',
+        verticalAlign: 'center'
+});
+searchArea.add(searchCategories);
+ 
+var searchSlider = Ti.UI.createSlider({
+        top: 113,
+        min: 1, max: 1000,
+        width: '94%',
+        tintColor: '#4cbbc8',
+        value: 80
+});
+ 
+var searchSliderLabel = Ti.UI.createLabel({
+        text:'Pris < ' + searchSlider.value + ' kr',
+        color:'#858688',
+        font:{fontFamily:'Lato-Regular', fontSize:defaultFontSize+3},
+        top: 78, left: 10,
+        height: 48,
+        textAlign: 'left',
+        verticalAlign: 'center'
+});
+ 
+searchSlider.addEventListener('change', function(e) {
+    searchSliderLabel.text = 'Pris < ' + String.format("%3.1f", e.value) + ' kr';
+});
+ 
+searchArea.add(searchSliderLabel);
+searchArea.add(searchSlider);
+ 
+win.add(searchArea);
+
+
+
+var tableData = [];
+ 
+var subHeader = Ti.UI.createTableViewRow({
+        backgroundColor: '#2e353f',
+        height:35
+});
+ 
+var subFlowLabel = Ti.UI.createLabel({
+        color:'white',
+        font:{fontFamily:'Lato-Bold', fontSize:defaultFontSize},
+        text:'Dessa borde du tycka asamycket om', //Bättre copy!
+        textAlign: 'center',
+        verticalAlign: 'center'
+});
+subHeader.add(subFlowLabel);
+ 
+tableData.push(subHeader);
+
+Ti.include('henke.js');
+ 
+function doenaInEnTillDryckBa(drinkId,drinkScore) {
+	
+        var row = Ti.UI.createTableViewRow({
+                className:'forumEvent', // used to improve table performance
+                selectedBackgroundColor:'white',
+                rowIndex:drinkId, // custom property, useful for determining the row during events
+                height:123
+        });
+       
+        var stretchBgRedwine = Ti.UI.createImageView({
+                image: 'img/stretchbg_redwine.jpg',
+                left:0, top:0,
+                width:7, height:123
+        });
+        row.add(stretchBgRedwine);
+		
+		if (monopolistsDatabase[drinkId].kat) { //Den här if:en behöver vara med för att alla drycker verkar in ha en kategori.
+        	var labelProductCategory = Ti.UI.createLabel({
+                	color:'#212a38',
+                	font:{fontFamily:'Lato-Bold', fontSize:defaultFontSize-3},
+                	text:monopolistsDatabase[drinkId].kat.toUpperCase(),
+            	    left:23, top: 19
+        	});
+        }
+       
+        row.add(labelProductCategory);
+ 
+        var labelProductName = Ti.UI.createLabel({
+                color:'#212a38',
+                font:{fontFamily:'Lato-Black', fontSize:defaultFontSize+4},
+                text:monopolistsDatabase[drinkId].namn.substr(0,27), //Lägg till "..." om den är längre än ca 27 tecken
+                left:23, top: 45
+        });
+        row.add(labelProductName);
+ 
+        var labelProductSubname = Ti.UI.createLabel({
+                color:'#212a38',
+                font:{fontFamily:'Lato-Black', fontSize:defaultFontSize+2},
+                text:monopolistsDatabase[drinkId].namn2, //Lägg till en "if .namn2 finns..." på denna.
+                left:23, top: 67
+        });
+        row.add(labelProductSubname);
+ 
+        var labelProductInfo = Ti.UI.createLabel({
+                color:'#72767c',
+                font:{fontFamily:'Lato-Bold', fontSize:defaultFontSize-3},
+                text:'PRIS ' + monopolistsDatabase[drinkId].pris[0] + ' kr    ALKOHOL ' + monopolistsDatabase[drinkId].alk + '    APK 1,0',
+                left:23, top: 97
+        });
+        row.add(labelProductInfo);
+ 
+        var flowArrow = Ti.UI.createImageView({
+                image: 'img/flowarrow.png',
+                right:10, top:53,
+                width:10, height:17
+        });
+        row.add(flowArrow);
+       
+        tableData.push(row);
 }
+ 
+var tableView = Ti.UI.createTableView({
+        backgroundColor:'white',
+        top: 220,
+        data:tableData
+});
+ 
+win.add(tableView);
+ 
+win.open();
 
-// This is a single context application with mutliple windows in a stack
-(function() {
-  //determine platform and form factor and render approproate components
-  var osname = Ti.Platform.osname,
-    version = Ti.Platform.version,
-    height = Ti.Platform.displayCaps.platformHeight,
-    width = Ti.Platform.displayCaps.platformWidth;
-
-    //considering tablets to have width over 720px and height over 600px - you can define your own
-  function checkTablet() {
-    var platform = Ti.Platform.osname;
-
-    switch (platform) {
-      case 'ipad':
-        return true;
-      case 'android':
-        var psc = Ti.Platform.Android.physicalSizeCategory;
-        var tiAndroid = Ti.Platform.Android;
-        return psc === tiAndroid.PHYSICAL_SIZE_CATEGORY_LARGE || psc === tiAndroid.PHYSICAL_SIZE_CATEGORY_XLARGE;
-      default:
-        return Math.min(
-          Ti.Platform.displayCaps.platformHeight,
-          Ti.Platform.displayCaps.platformWidth
-        ) >= 400;
-    }
-  }
-
-  var isTablet = checkTablet();
-  console.log(isTablet);
-
-   var Window;
-   if (isTablet) {
-     Window = require('ui/tablet/ApplicationWindow');
-   } else {
-     // iPhone and Mobile Web make use of the platform-specific navigation controller,
-     // all other platforms follow a similar UI pattern
-     if (osname === 'iphone') {
-       //Window = require('ui/handheld/ios/ApplicationWindow');
-       Window = require('ui/handheld/android/ApplicationWindow');
-     } else if (osname == 'mobileweb') {
-       //Window = require('ui/handheld/mobileweb/ApplicationWindow');
-       Window = require('ui/handheld/android/ApplicationWindow');
-     } else {
-       Window = require('ui/handheld/android/ApplicationWindow');
-     }
-   }
-   new Window().open();
-})();
--- Resources/ui/handheld/android/ApplicationWindow.js
+++ Resources/ui/handheld/android/ApplicationWindow.js
@@ -1,71 +1,19 @@
 //Application Window Component Constructor
 function ApplicationWindow() {
 	//load component dependencies
-	var DraggingRow = require('ui/common/DraggingRow');
-	var DrinkRowItem = require('ui/common/DrinkRowItem');
-	var DrinkRowStar = require('ui/common/DrinkRowStar');
-
-	var snapTresholdFraction = 0.5;
-	var snapOpenFraction = 0.65;
-
+	var FirstView = require('ui/common/FirstView');
+		
 	//create component instance
 	var self = Ti.UI.createWindow({
-		backgroundColor : '#FFFFFF',
-		fullscreen : false,
-		navBarHidden : true
+		backgroundColor:'#ffffff',
+		navBarHidden:true,
+		exitOnClose:true
 	});
-
-	//------------------------
-	//header
-	var header = Ti.UI.createView({
-        backgroundColor: '#232a35',
-        height: '59dip',
-        top: 0
-	});
-	var flowLabel = Ti.UI.createLabel({
-		color : 'white',
-		font : {
-			fontFamily : 'Lato-Bold',
-			fontSize : defaultFontSize + 2 + 'dip'
-		},
-		text : 'Flöde',
-		textAlign : 'center',
-		bottom : '14dip'
-	});
-	header.add(flowLabel);
-	self.add(header);
-
-	//sub header
-	var subHeader = Ti.UI.createView({
-		backgroundColor : '#2e353f',
-		height : '35dip',
-		top: '59dip'
-	});
-
-	var subFlowLabel = Ti.UI.createLabel({
-		color : 'white',
-		font : {
-			fontFamily : 'Lato-Bold',
-			fontSize : defaultFontSize+'dip'
-		},
-		text : 'Dessa borde du tycka asamycket om', //Bättre copy!
-		textAlign : 'center',
-		verticalAlign : 'center'
-	});
-	subHeader.add(subFlowLabel);
-	self.add(subHeader);
-
-	//table
-	var tableData = [];
-
-	tableData.push(new DraggingRow(new DrinkRowStar(), new DrinkRowItem(), snapTresholdFraction, snapOpenFraction));
-	var tableView = Ti.UI.createTableView({
-		backgroundColor : 'white',
-		top : '94dip',
-		data : tableData
-	});
-	self.add(tableView);
-
+		
+	//construct UI
+	var firstView = new FirstView();
+	self.add(firstView);
+	
 	return self;
 }
 
